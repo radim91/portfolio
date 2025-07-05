7b6f9293e3c49427e793931a411211531497ccd9
Radim Hejduk <dev@radimhejduk.cz>
2025-07-05 13:52:23 +0200

    portfolio update - my work section

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("ahoj");
+    gsap.from('.hero-image', {
+        opacity: 0.5
+    });
+
+    gsap.to('.hero-image', {
+        x: -20,
+        duration: 1,
+        opacity: 1
+    });
+
+    document.getElementById("work-link").addEventListener("mouseover", () => {
+        linkHover('#work-stick');
+    })
+
+    document.getElementById("bison-link").addEventListener("mouseover", () => {
+        linkHover('#bison-stick');
+    })
+
+    function linkHover(id) {
+        let tl = gsap.timeline();
+        tl.to(id, { x: 5, y: -5 });
+        tl.to(id, { x: 0, y: 0 }, 0.1);
+    }
 });
